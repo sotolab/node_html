899f59783f4dfec97539309c5b41e17b11dce11b
sotojang <mrjang28@gmail.com>
2019-08-27 17:32:45 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var app = express();
 var router = require('./router/main')(app);
+let port = 3000;
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 app.use(express.static('public'));
 
-var server = app.listen(3000, function(){
-    console.log("Express server has started on port 3000")
+var server = app.listen(port, function(){
+    console.log("Express server has started on port "+ port)
 });
